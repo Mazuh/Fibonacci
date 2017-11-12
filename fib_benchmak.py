30d9489ca6b31b8b0f2c7ac7528b3759c2dd917e
Marcell Guilherme Costa da Silva <Mazuh@users.noreply.github.com>
2017-11-12 00:24:19 -0300

    Update fib_benchmak.py

--- fib_benchmak.py
+++ fib_benchmak.py
@@ -3,17 +3,17 @@ from timeit import timeit
 
 prefix = 'import fib; '
 with open('./report.csv', 'w') as reportfile:
-  writer = csv.writer(reportfile)
-  writer.writerow([
-    'n_arg',
-    'recursive_time',
-    'iterative_time',
-    'explicit_time'
-  ])
-  for n in range(1, 40):
-    print('n=', n, '...', end='')
-    rec = timeit('{}fib.f_recursive({})'.format(prefix, n))
-    it = timeit('{}fib.f_iterative({})'.format(prefix, n))
-    exp = timeit('{}fib.f_explicit({})'.format(prefix, n))
-    print(' OK')
-    writer.writerow([n, rec, it, exp])
+    writer = csv.writer(reportfile)
+    writer.writerow([
+        'n_arg',
+        'recursive_time',
+        'iterative_time',
+        'explicit_time'
+    ])
+    for n in range(1, 40):
+        print('n=', n, '...', end='')
+        rec = timeit('{}fib.f_recursive({})'.format(prefix, n))
+        it = timeit('{}fib.f_iterative({})'.format(prefix, n))
+        exp = timeit('{}fib.f_explicit({})'.format(prefix, n))
+        print(' OK')
+        writer.writerow([n, rec, it, exp])
